1adf86630d60f1f8f8d164cf4702134e183b3ac3
erdil <erdil1845@gmail.com>
2024-04-20 18:23:43 +0300

    Erdil 20.04.2024 V02

---
+++ src/test/java/tests/C17_BaseUrlHerOkuAppQueryParam.java
@@ -0,0 +1,34 @@
+package tests;
+
+import baseUrl.BaseUrlHerOkuApp;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class C17_BaseUrlHerOkuAppQueryParam extends BaseUrlHerOkuApp {
+/*
+
+        "https://restful-booker.herokuapp.com/booking endpointine
+        gerekli Query parametrelerini yazarak
+            “firstname” degeri “Jim” olan rezervasyon oldugunu
+        test edecek bir GET request gonderdigimizde,
+        donen response’un
+            status code’unun 200 oldugunu
+            // ve “Jim” ismine sahip 4 booking oldugunu test edin
+
+         */
+
+    @Test
+    public void test01(){
+        specHerOkuApp.pathParam("pp1","booking").queryParam("firstname","Jim");
+
+        Response response=given().when().spec(specHerOkuApp).get("/{pp1}");
+
+        response.then().assertThat().statusCode(200).body("size()",equalTo(12));
+
+
+
+    }
+}
