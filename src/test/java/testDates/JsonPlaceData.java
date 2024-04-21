14c59e44e64e5448222a2762cb17ed4f74661ef7
erdil <erdil1845@gmail.com>
2024-04-21 14:18:26 +0300

    Erdil 21.04.2024 V03

--- src/test/java/testDates/JsonPlaceData.java
+++ src/test/java/testDates/JsonPlaceData.java
@@ -1,5 +1,7 @@
 package testDates;
 
+import org.json.JSONObject;
+
 public class JsonPlaceData {
         /*
    https://jsonplaceholder.typicode.com/posts/22 url'ine
@@ -18,4 +20,33 @@ public class JsonPlaceData {
        "body":"eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
    }
 */
+
+    public static int basariliSC=200;
+    public static String contentType="application/json; charset=utf-8";
+    public static String header="keep-alive";
+    public static String header2="cloudflare";
+
+    public static JSONObject expectedDataOlustur01() {
+        JSONObject expData = new JSONObject();
+        expData.put("userId", 3);
+        expData.put("id", 22);
+        expData.put("title", "dolor sint quo a velit explicabo quia nam");
+        expData.put("body", "eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\n" +
+                "expedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse");
+        return expData;
+    }
+
+    public static JSONObject JsonDataOlustur(int userId, int id,String title,String body){
+        JSONObject expData=new JSONObject();
+        expData.put("userId",userId);
+        expData.put("id",id);
+        expData.put("title",title);
+        expData.put("body",body);
+
+        return expData;
+    }
+
+
+
+
 }
---
+++ src/test/java/tests/C20_TestDataJsonPlaceDinamik.java
@@ -0,0 +1,52 @@
+package tests;
+
+import baseUrl.BaseUrlJsonPlaceHolder;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Test;
+import testDates.JsonPlaceData;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class C20_TestDataJsonPlaceDinamik extends BaseUrlJsonPlaceHolder {
+    /*
+    https://jsonplaceholder.typicode.com/posts/40 url'ine
+    bir GET request yolladigimizda
+    donen response'in
+        status kodunun 200
+        ve response body'sinin asagida verilen ile ayni oldugunu test ediniz
+
+    Response body :
+    {
+        "userId":4,
+        "id":40,
+        "title":"enim quo cumque",
+        "body":"ut voluptatum aliquid illo tenetur nemo sequi quo facilis\nipsum rem optio mollitia quas\nvoluptatem eum voluptas qui\nunde omnis voluptatem iure quasi maxime voluptas nam"
+    }
+ */
+    @Test
+    public void test02(){
+        //1
+        specJsonPlaceHolder.pathParams("pp1","posts","pp2","65");
+
+        //2-ExpBody oluştur
+        JSONObject expBody= JsonPlaceData.JsonDataOlustur(7,65,"consequatur id enim sunt et et","voluptatibus ex esse\nsint explicabo est aliquid cumque adipisci fuga repellat labore\nmolestiae corrupti ex saepe at asperiores et perferendis\nnatus id esse incidunt pariatur");
+
+        //3-
+        Response response=given().when().spec(specJsonPlaceHolder).get("/{pp1}/{pp2}");
+
+        //4-
+        JsonPath resJP=response.jsonPath();
+
+        assertEquals(JsonPlaceData.basariliSC,response.getStatusCode());
+        assertEquals(JsonPlaceData.header,response.getHeader("Connection"));
+        assertEquals(expBody.getInt("userId"),resJP.getInt("userId"));
+        assertEquals(expBody.getInt("id"),resJP.getInt("id"));
+        assertEquals(expBody.getString("title"),resJP.getString("title"));
+        assertEquals(expBody.getString("body"),resJP.getString("body"));
+
+
+    }
+}
